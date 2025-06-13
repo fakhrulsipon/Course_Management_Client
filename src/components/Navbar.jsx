47050348b36f4fa7b0c66a76bc4c1e3e7b3af21f
sipon <fakhrulislamsipon@gmail.com>
2025-06-13 23:31:09 +0600

    implement dropdown menu navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,49 +3,59 @@ import { Link, NavLink } from 'react-router';
 import { AuthContext } from '../Provider/AuthProvider';
 
 const Navbar = () => {
-  const {user, logoutUser } = use(AuthContext)
+  const { user, logoutUser } = use(AuthContext)
   const hangleLogout = () => {
     logoutUser()
-    .then(() => {
-      alert('sign out successfully')
-    })
-    .catch(error => {
-      console.log(error)
-    })
+      .then(() => {
+        alert('sign out successfully')
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
-    return (
-       <div className="navbar bg-base-100 shadow-sm">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            <li><NavLink>Home</NavLink></li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">Course Management </a>
       </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><NavLink>Home</NavLink></li>
-      </ul>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li><NavLink>Home</NavLink></li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        {
+          user ? <>
+            <div className="dropdown dropdown-center">
+              <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <img src={user?.photoURL} alt="User" />
+                </div>
+              </div>
+              <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box font-bold text-xl">
+                <li><Link onClick={hangleLogout}>Logout</Link></li>
+              </ul>
+            </div>
+          </> :
+            <>
+              <Link className='btn mr-3' to='/login'>Login</Link>
+              <Link className='btn' to='/register'>Register</Link>
+            </>
+        }
+      </div>
     </div>
-    <a className="btn btn-ghost text-xl">Course Management </a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-     <li><NavLink>Home</NavLink></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-   {
-    user? <>
-      <Link onClick={hangleLogout} className='btn'>Logout</Link>
-    </> :
-    <>
-    <Link className='btn mr-3' to='/login'>Login</Link>
-    <Link className='btn' to='/register'>Register</Link>
-    </>
-   }
-  </div>
-</div>
-    );
+  );
 };
 
 export default Navbar;
+
